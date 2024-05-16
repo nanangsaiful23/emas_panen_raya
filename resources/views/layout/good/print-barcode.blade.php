b338cbd678cc2bb1c9f28f2c282f8e17ed5f8a8c
nisadyo <riyaniannisa@yahoo.com>
2024-05-16 12:49:47 +0700

    rearrange print

--- resources/views/layout/good/print-barcode.blade.php
+++ resources/views/layout/good/print-barcode.blade.php
@@ -52,7 +52,7 @@
 				            <div style="text-align: center; height: 7mm;">
 				            	@if($goods[$i * 2]['stone_weight'] != '0.00' && $goods[$i * 2]['stone_weight'] != null && $goods[$i * 2]['stone_weight'] != '')
 					            	<div style="font-size: 12px; text-align: center; margin-top: 2mm;">
-					            		<b>{{ $goods[$i * 2]['weight'] . ' ' . $goods[$i * 2]['old_gold'] }}</b><span style="font-size: 6px !important">gr</span>
+					            		<b>{{ $goods[$i * 2]['weight'] }}</b><span style="font-size: 6px !important">{{ $goods[$i * 2]['old_gold'] }} gr</span>
 					            	</div>
 					            	<div style="font-size: 6px; text-align: center;">
 					            		<b>{{ 'BATU ' . $goods[$i * 2]['stone_weight'] }}</b><span style="font-size: 6px !important">gr</span>
@@ -62,7 +62,7 @@
 					            	</div>
 					            @else
 					            	<div style="font-size: 12px; text-align: center; margin-top: 3mm;">
-					            		<b>{{ $goods[$i * 2]['weight'] . ' ' . $goods[$i * 2]['old_gold'] }}</b><span style="font-size: 6px !important">gr</span>
+					            		<b>{{ $goods[$i * 2]['weight'] }}</b><span style="font-size: 6px !important">{{ $goods[$i * 2]['old_gold'] }} gr</span>
 					            	</div>
 						        @endif
 				            	<div style="font-size: 8px; text-align: center;">
@@ -85,7 +85,7 @@
 					            <div style="text-align: center; height: 7mm;">
 					            	@if($goods[$i * 2 + 1]['stone_weight'] != '0.00' && $goods[$i * 2 + 1]['stone_weight'] != null && $goods[$i * 2 + 1]['stone_weight'] != '')
 						            	<div style="font-size: 12px; text-align: center; margin-top: 2mm;">
-						            		<b>{{ $goods[$i * 2 + 1]['weight'] . ' ' . $goods[$i * 2 + 1]['old_gold'] }}</b><span style="font-size: 6px !important">gr</span>
+						            		<b>{{ $goods[$i * 2 + 1]['weight'] }}</b><span style="font-size: 6px !important">{{ $goods[$i * 2 + 1]['old_gold'] }} gr</span>
 						            	</div>
 						            	<div style="font-size: 6px; text-align: center;">
 						            		<b>{{ 'BATU ' . $goods[$i * 2 + 1]['stone_weight'] }}</b><span style="font-size: 6px !important">gr</span>
@@ -95,7 +95,7 @@
 						            	</div>
 						            @else
 						            	<div style="font-size: 12px; text-align: center; margin-top: 3mm;">
-						            		<b>{{ $goods[$i * 2 + 1]['weight'] . ' ' . $goods[$i * 2 + 1]['old_gold'] }}</b><span style="font-size: 6px !important">gr</span>
+						            		<b>{{ $goods[$i * 2 + 1]['weight'] }}</b><span style="font-size: 6px !important">{{ $goods[$i * 2 + 1]['old_gold'] }} gr</span>
 						            	</div>
 							        @endif
 					            	<div style="font-size: 8px; text-align: center;">
